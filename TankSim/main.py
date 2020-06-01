6acd086b44c75882d9c567f3871d0abdfb5bc79d
Joco-San <joccopalica@gmail.com>
2020-06-01 15:39:12 +0200

    Tank ground

--- TankSim/main.py
+++ TankSim/main.py
@@ -9,6 +9,8 @@ ARENA_LENGTH = 80
 ARENA_HEIGHT = 20
 G = 9.81
 
+screen_buffer = []
+turn = 1
 
 def clear_screen():
     if name == "nt":
@@ -18,20 +20,43 @@ def clear_screen():
 
 
 def create_screen_buffer(height, width):
-    matrix = []
     for row in range(height):
-        matrix.append([])
+        screen_buffer.append([])
         for _ in range(width):
-            matrix[row].append(" ")
-    return matrix
+            screen_buffer[row].append(" ")
 
 
-def init_game(screen_buffer):
+def drop_place(posx, char):
+    posx -= 1
+    if posx < 0 or posx > ARENA_LENGTH - 1: raise Exception("Invalid x position")
+    for i in range(ARENA_HEIGHT - 1):
+        peek_pixel = screen_buffer[i + 1][posx]
+        if peek_pixel == chr(0x2588):
+            screen_buffer[i][posx] = char
+            return ARENA_HEIGHT - i
 
 
+def init_game():
+    t_player = {
+        "x": randint(1, ARENA_LENGTH / 2 - 1),
+        "y": None
+    }
+
+    t_pc = {
+        "x": randint(ARENA_LENGTH / 2, ARENA_LENGTH),
+        "y": None
+    }
+
+    for i in range(ARENA_LENGTH):
+        screen_buffer[ARENA_HEIGHT - 1][i] = chr(0x2588)
+
+    t_player["y"] = drop_place(t_player["x"], "H")
+    t_pc["y"]     = drop_place(t_pc["x"], "P")
+
+    return (t_player, t_pc)
 
 
-def render_game(screen_buffer):
+def render_game():
     print(" " + "".join(["_"] * ARENA_LENGTH))
     for row in screen_buffer:
         print("|" + "".join(row) + "|")
@@ -39,13 +64,15 @@ def render_game(screen_buffer):
 
 
 def game(t_player, t_pc):
-    pass
+    print(t_player["x"], t_player["y"])
+    print(t_pc["x"], t_pc["y"])
 
 
 def main():
-    screen_buffer = create_screen_buffer(ARENA_HEIGHT, ARENA_LENGTH)
-    tank_player, tank_pc = init_game(screen_buffer)
-    render_game(screen_buffer)
+    create_screen_buffer(ARENA_HEIGHT, ARENA_LENGTH)
+    t_player, t_pc = init_game()
+    render_game()
+    game(t_player, t_pc)
 
 
 if __name__ == "__main__":
