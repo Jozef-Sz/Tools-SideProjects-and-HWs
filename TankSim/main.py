0da78f6fbd391646291e87480115e07d004a1695
Jozef-Sz <joccopalica@gmail.com>
2020-06-01 22:16:19 +0200

    projectile drawing

--- TankSim/main.py
+++ TankSim/main.py
@@ -26,23 +26,35 @@ def create_screen_buffer(height, width):
             screen_buffer[row].append(" ")
 
 
-def drop_place(posx, char):
-    posx -= 1
-    if posx < 0 or posx > ARENA_LENGTH - 1: raise Exception("Invalid x position")
+def drop_place(xpos, char):
+    xpos -= 1
+    if xpos < 0 or xpos > ARENA_LENGTH - 1: raise Exception("Invalid x position")
     for i in range(ARENA_HEIGHT - 1):
-        peek_pixel = screen_buffer[i + 1][posx]
+        peek_pixel = screen_buffer[i + 1][xpos]
         if peek_pixel == chr(0x2588):
-            screen_buffer[i][posx] = char
+            screen_buffer[i][xpos] = char
             return ARENA_HEIGHT - i
 
 
+def insert_char(xpos, ypos, char):
+    print("Inside insert char")
+    if xpos < 0 or ypos < 0:
+        return
+    print("Passed first filter")
+    x = xpos - 1
+    y = ARENA_HEIGHT - ypos if ypos != 0 else -1
+    print("x of f", x, "y of f", y)    
+    if (x < 0 or x > ARENA_LENGTH - 1) or (y < 0 or y > ARENA_HEIGHT - 1):
+        return
+    print("Passed second filter")
+    screen_buffer[y][x] = char
+
+
 def init_game():
-    t_player = {
+    t_player, t_pc = {
         "x": randint(1, ARENA_LENGTH / 2 - 1),
         "y": None
-    }
-
-    t_pc = {
+    }, {
         "x": randint(ARENA_LENGTH / 2, ARENA_LENGTH),
         "y": None
     }
@@ -63,16 +75,42 @@ def render_game():
     print("-" + "".join(["-"] * ARENA_LENGTH) + "-")
 
 
+def shoot_function(x, angle, velocity, xoffset, yoffset):
+    velocity /= 20  # Normalize velocity
+    a = tan(radians(angle)) * (x + xoffset)
+    b = G * ((x + xoffset)**2)
+    c = 2 * (velocity**2) * (cos(radians(angle))**2)
+    b /= c
+    return round(a - b) + yoffset 
+
+
+def create_projectile_trajectory(angle, velocity, player):
+    for x in range(1, ARENA_LENGTH + 1):
+        y = shoot_function(x, angle, velocity, -player["x"], player["y"])
+        print(x, y)
+        insert_char(x, y, ".")
+    return 0
+
+
+def human_turn(pos):
+    create_projectile_trajectory(45, 300, pos)
+
+
+def pc_turn(pos):
+    pass
+
+
 def game(t_player, t_pc):
+    human_turn(t_player)
+    render_game()
     print(t_player["x"], t_player["y"])
-    print(t_pc["x"], t_pc["y"])
+    return 0
 
 
 def main():
     create_screen_buffer(ARENA_HEIGHT, ARENA_LENGTH)
     t_player, t_pc = init_game()
-    render_game()
-    game(t_player, t_pc)
+    winner = game(t_player, t_pc)
 
 
 if __name__ == "__main__":
