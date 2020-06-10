517446e1ecba24a190f6e223347c04cd259004b8
Joco-San <joccopalica@gmail.com>
2020-06-10 16:38:13 +0200

    changed string metadatas type to pointer and refactored strlib functions, then implemented stradd, strcopy and contains functions

--- strlib/src/sandbox.c
+++ strlib/src/sandbox.c
@@ -4,23 +4,58 @@
 int main() 
 {
     string name = str("Joco");
-
     printf("Here is our string: %s|\n", name.str);
-    printf("%d\n", name.length);
+    printf("Length %d\n", *name.length);
+    printf("Capacity %d\n", *name.capacity);
 
-    name = strpush(name, " is");
+    // strpush(name, " is");
+    // printf("Here is our string: %s|\n", name.str);
+    // printf("Length %d\n", *name.length);
+    // printf("Capacity %d\n", *name.capacity);
+
+    // strpush(name, " the b");
+    // printf("Here is our string: %s|\n", name.str);
+    // printf("Length %d\n", *name.length);
+    // printf("Capacity %d\n", *name.capacity);
+
+    // string the = substr(name, 8, 10);
+    // printf("Here is out substring: |%s|\n", the.str);
+    // printf("Length %d\n", *the.length);
+    // printf("Capacity %d\n", *the.capacity);
+
+    // string pika = str("Pika");
+    // printf("Here is our string: %s|\n", pika.str);
+    // printf("Length %d\n", *pika.length);
+    // printf("Capacity %d\n", *pika.capacity);
+    // string boo = str("boo");
+    // printf("Here is our string: %s|\n", boo.str);
+    // printf("Length %d\n", *boo.length);
+    // printf("Capacity %d\n", *boo.capacity);
+    // stradd(pika, boo);
+    // printf("Here is our string: %s|\n", pika.str);
+    // printf("Length %d\n", *pika.length);
+    // printf("Capacity %d\n", *pika.capacity);
+    
+    string namecopy = strcopy(name);
+    printf("Here is our string: %s|\n", namecopy.str);
+    printf("Length %d\n", *namecopy.length);
+    printf("Capacity %d\n", *namecopy.capacity);
+
+    strpush(namecopy, " hello");
+    printf("Here is our string: %s|\n", namecopy.str);
+    printf("Length %d\n", *namecopy.length);
+    printf("Capacity %d\n", *namecopy.capacity);
     printf("Here is our string: %s|\n", name.str);
-    printf("%d\n", name.length);
+    printf("Length %d\n", *name.length);
+    printf("Capacity %d\n", *name.capacity);
 
-    name = strpush(name, " the best");
-    printf("Here is our string: %s|\n", name.str);
-    printf("%d\n", name.length);
+    printf("%d", contains(namecopy, "o"));
 
-    printf("%s\n", name.str);
 
     strdel(name);
-
-    printf("%s", name.str);
-
+    strdel(namecopy);
+    // strdel(the);
+    // strdel(pika);
+    // strdel(boo);
     return 0;
 }
