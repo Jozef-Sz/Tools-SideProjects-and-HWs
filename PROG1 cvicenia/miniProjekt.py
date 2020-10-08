b6b05478321be98547aeaf78435c28fb9bca4c04
Jozef-Sz <joccopalica@gmail.com>
2020-10-08 12:03:49 +0200

    miniProjekt refactored a bit and added docstrings

--- PROG1 cvicenia/miniProjekt.py
+++ PROG1 cvicenia/miniProjekt.py
@@ -135,64 +135,163 @@ def arc(t, r, angle):
   polyline(t, n, step_length, step_angle)
 
 
-def perpendicular_arc(t, height, angle):
-  r =  (height / 2) / sin(radians(angle / 2))
-  arc(t, r, angle)
-
-
 def petal(t, r, angle):
+  """
+  Draws a petal using two arcs.
+  
+  t:     Turtle
+  r:     radius of the arcs
+  angle: angle (degrees) that subtends the arcs
+  """
   for _ in range(2):
     arc(t, r, angle)
     t.lt(180 - angle)
 
 
+def perpendicular_arc(t, height, angle):
+  """
+  Draw an arc with it's end points aligned on top of each other, perpedicular to ground.
+
+  t:      an instance of Turtle()
+  height: distance between the ends of the arc
+  angle:  sets the depth of the curve
+  """
+  # Calculates "preponu pravouhleho trojuholnika v kruhu" a.k.a radius of the circle
+  r =  (height / 2) / sin(radians(angle / 2))
+  # Draw an arc
+  arc(t, r, angle)
+
+
 def flower_stem(t, height, angle):
+  """
+  Draws a stem based on a height and an angle value.
+
+  t:      an instance of Turtle()
+  height: height of the stem from the center of flower, to the bottom of the stem
+  angle:  angle of the curve in stem
+  """
+  # Set start position
   t.rt(90 + angle / 2)
+  # Draw an arc with it's end points aligned on top of each other
   perpendicular_arc(t, height, angle)
+  # Reset position
   t.lt(180 - angle / 2)
 
 
 def flower_leafs(t, length, width, angle_ground):
+  """
+  Draws two mirrored leafs with a particular length, width and an angle from ground.
+
+  t:            an instance of Turtle()
+  length:       length of leafs from the begining, to the tip
+  width:        width of leafs
+  angle_ground: relative angle of leafs from the ground
+  """
+  # Set t to start position for drawing
   t.lt(90 - angle_ground - width / 2)
+  # Draw a leaf with two arcs
   for _ in range(2):
     perpendicular_arc(t, length, width)
     t.lt(180 - width)
 
-  t.rt(90 - angle_ground - width / 2)
-  t.rt(90 - angle_ground + width / 2)
+  # Set t to start position for second leaf
+  t.rt(180 - 2 * angle_ground)
+  # Draw a leaf with two arcs
   for _ in range(2):
     perpendicular_arc(t, length, width)
     t.lt(180 - width)
 
-  t.lt(90 - angle_ground + width / 2)
+  # Reset turtle position
+  t.lt(-angle_ground + width / 2)
 
 
-def move_right(t, d):
+def move(t, x, y=0):
+  """
+  Moves an instance of Turtle() by a x and y value relative to it's current position.
+
+  t: an instance of Turtle()
+  x: distance on x axis
+  y: distance on y axis
+  """
+  current_pos = t.pos()
   t.pu()
-  t.rt(90)
-  t.fd(d)
+  t.setpos(current_pos[0] + x, current_pos[1] + y)
   t.pd()
 
 
 def flower(t, n_petal, r_petal, angle_petal, height_stem, angle_stem, length_leaf, width_leaf, angle_ground):
+  """
+  Draws a complete flower with a stem and leafs.
+  
+  t:            an instance of Turtle()
+  n_petal:      number of petals of the flower
+  r_petal:      radius of half petal
+  angle_petal:  angle of the half petal arc
+  height_stem:  height of the stem from the center of flower, to the bottom of the stem
+  angle_stem:   angle of the curve in stem
+  length_leaf:  length of leafs from the begining, to the tip
+  width_leaf:   width of leafs
+  angle_ground: relative angle of leafs from the ground
+  """
+  # Draw petals
   for _ in range(n_petal):
     petal(t, r_petal, angle_petal)
     t.lt(360 / n_petal)
+  # Draw stem
   flower_stem(t, height_stem, angle_stem)
+  # Draw leafs
   flower_leafs(t, length_leaf, width_leaf, angle_ground)
-  t.pu()
-  t.forward(height_stem)
-  t.pd()
+  # Reset turtle position
+  move(t, 0, height_stem)
 
 
-
+# Create turtle instance 
 bob = turtle.Turtle()
 
+# Initial settings
 # bob.speed(0)
 bob._tracer(0)
-flower(bob, n_petal=7, r_petal=80, angle_petal=55, height_stem=150, angle_stem=80, length_leaf=50, width_leaf=80, angle_ground=45)
-move_right(bob, 200)
-flower(bob, n_petal=10, r_petal=60, angle_petal=60, height_stem=300, angle_stem=50, length_leaf=150, width_leaf=10, angle_ground=75)
-move_right(bob, 200)
-flower(bob, n_petal=20, r_petal=300, angle_petal=10, height_stem=165, angle_stem=65, length_leaf=150, width_leaf=120, angle_ground=45)
+move(bob, -200)
+
+# Drawing flowers
+flower(
+  bob, 
+  n_petal=7, 
+  r_petal=80, 
+  angle_petal=55, 
+  height_stem=150, 
+  angle_stem=95, 
+  length_leaf=50, 
+  width_leaf=80, 
+  angle_ground=45
+)
+
+move(bob, 200)
+flower(
+  bob, 
+  n_petal=10, 
+  r_petal=50, 
+  angle_petal=85, 
+  height_stem=300, 
+  angle_stem=20, 
+  length_leaf=150, 
+  width_leaf=10, 
+  angle_ground=75
+)
+
+move(bob, 200)
+flower(
+  bob, 
+  n_petal=20, 
+  r_petal=300, 
+  angle_petal=10, 
+  height_stem=165, 
+  angle_stem=65, 
+  length_leaf=150, 
+  width_leaf=120, 
+  angle_ground=45
+)
+
+
+
 turtle.done()
